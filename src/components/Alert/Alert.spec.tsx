836ae061586712a3680c3c7e43aa1104a16a4160
ameerthehacker <ameerjhanprof@gmail.com>
2018-12-28 18:46:06 +0530

    feat(Alert): add dismissible alerts

---
+++ src/components/Alert/Alert.spec.tsx
@@ -0,0 +1,26 @@
+import { mount } from "enzyme";
+import * as React from "react";
+import Alert, { AlertCloseBtnStyled, AlertStyled } from "./Alert";
+
+describe("Alert", () => {
+  it("should dismiss the alert when close button is clicked", () => {
+    const component = mount(<Alert dismissible={true} />);
+    const alertCloseBtnComponent = component.find(AlertCloseBtnStyled);
+
+    expect(component.find(AlertStyled).exists()).toBeTruthy();
+
+    alertCloseBtnComponent.simulate("click");
+
+    expect(component.find(AlertStyled).exists()).toBeFalsy();
+  });
+
+  it("should trigger the onDismiss callback when close button is clicked", () => {
+    const cb = jest.fn();
+    const component = mount(<Alert dismissible={true} onDismiss={cb} />);
+    const alertCloseBtnComponent = component.find(AlertCloseBtnStyled);
+
+    alertCloseBtnComponent.simulate("click");
+
+    expect(cb).toBeCalled();
+  });
+});
--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -4,9 +4,23 @@ import styled from "../../styled-components";
 import { defaultTheme, ITheme } from "../../theme";
 import { space } from "../../theme/variables";
 
+// There is two interfaces to avoid the warning `unknown event handler`
+
+interface IAlertStyledProps {
+  variant?: "primary" | "secondary" | "warning" | "danger" | "success";
+  theme?: ITheme;
+  dismissible?: boolean;
+}
+
 interface IAlertProps {
   variant?: "primary" | "secondary" | "warning" | "danger" | "success";
   theme?: ITheme;
+  dismissible?: boolean;
+  onDismiss?: (evt: Alert) => void;
+}
+
+interface IAlertState {
+  isVisible: boolean;
 }
 
 const alerts = variant({
@@ -14,16 +28,59 @@ const alerts = variant({
   prop: "variant",
 });
 
-const AlertStyled = styled("div")<IAlertProps>`
+export const AlertStyled = styled("div")<IAlertStyledProps>`
   ${alerts}
   padding: ${space[2]}px
 `;
 
+export const AlertCloseBtnStyled = styled("span")`
+  float: right;
+  cursor: pointer;
+`;
+
 AlertStyled.defaultProps = {
+  dismissible: false,
   theme: defaultTheme,
   variant: "primary",
 };
 
-const Alert = (props: IAlertProps) => <AlertStyled {...props} />;
+export default class Alert extends React.Component<IAlertProps, IAlertState> {
+  constructor(props: IAlertProps) {
+    super(props);
+    this.state = { isVisible: true };
+    this.dismissAlert = this.dismissAlert.bind(this);
+  }
 
-export default Alert;
+  public dismissAlert() {
+    this.setState({
+      isVisible: false,
+    });
+    if (this.props.onDismiss !== undefined) {
+      this.props.onDismiss(this);
+    }
+  }
+
+  public render() {
+    const dismissBtn = this.props.dismissible ? (
+      <AlertCloseBtnStyled onClick={this.dismissAlert}>
+        &times;
+      </AlertCloseBtnStyled>
+    ) : (
+      ""
+    );
+    const alert = this.state.isVisible ? (
+      <AlertStyled
+        variant={this.props.variant}
+        theme={this.props.theme}
+        dismissible={this.props.dismissible}
+      >
+        {this.props.children}
+        {dismissBtn}
+      </AlertStyled>
+    ) : (
+      ""
+    );
+
+    return alert;
+  }
+}
